cbf77a27bb737ac408fbca4c06be285a152a4c36
Ahmed Baig <ahmedbaig54@yahoo.com>
2019-04-23 13:56:27 +0500

    app fix

--- src/app/auth/auth-service.service.ts
+++ src/app/auth/auth-service.service.ts
@@ -9,39 +9,40 @@ import { Observable } from 'rxjs';
 })
 export class AuthServiceService {
   user: any = {};
-  logged: Boolean = false
-  origin:String = getOrigin();
-  constructor(private http:HttpClient,private route:Router) { }
+  logged: Boolean = false;
+  origin: String = getOrigin();
+  constructor(private http: HttpClient, private route: Router) { }
 
-   isAuthenticated(){
-    if(localStorage.getItem('session_t')!=null){ 
-      this.isTokenExpired(localStorage.getItem('session_t'))
-    }else{
+   isAuthenticated() {
+    if (localStorage.getItem('session_t') != null) {
+      this.isTokenExpired(localStorage.getItem('session_t'));
+    } else {
       this.clear();
-      this.route.navigate(['/auth'])
+      this.route.navigate(['/auth']);
     }
   }
 
-  isTokenExpired(token:any){
-    this.verifyToken(token).subscribe(res=>{
-      if(res.success){ 
+  isTokenExpired(token: any) {
+    this.verifyToken(token).subscribe(res => {
+      if (res.success) {
         this.user = res.user;
-        this.logged = true
-        this.route.navigate(['/'])
-      } 
-      this.clear();
-      this.route.navigate(['/auth'])
-      return false;
-    })
+        this.logged = true;
+        this.route.navigate(['/']);
+      } else {
+        this.clear();
+        this.route.navigate(['/auth']);
+        return false;
+      }
+    });
   }
 
-  clear(){
+  clear() {
     this.logged = false,
-    localStorage.removeItem('session_t')
+    localStorage.removeItem('session_t');
     this.user = {};
   }
 
-  verifyToken(token:String):Observable<any>{
-    return this.http.get(this.origin+"/api/userSession/verify?token="+token)
+  verifyToken(token: String): Observable<any> {
+    return this.http.get(this.origin + '/api/userSession/verify?token=' + token);
   }
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthServiceService } from 'src/app/auth/auth-service.service';
 declare var $: any;
+declare var app:any;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -13,34 +14,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this._auth.isAuthenticated();
-
-    this.sideNav(),
-    this.sideNavToggle(),
-    this.sidePanelToggle();
-  }
-
-  sideNav() {
-    $('.side-nav .side-nav-menu li a').on('click', function(e:any) {
-        $(this).parent().hasClass('open') ? $(this).parent().children('.dropdown-menu').slideUp(200, function() {
-            $(this).parent().removeClass('open');
-        }) : ($(this).parent().parent()
-        .children('li.open')
-        .children('.dropdown-menu').slideUp(200), $(this).parent().parent().children('li.open').children('a').removeClass('open'), $(this).parent().parent().children('li.open').removeClass('open'), $(this).parent().children('.dropdown-menu').slideDown(200, function() {
-            $(this).parent().addClass('open');
-        }));
-      });
-  }
-
-  sideNavToggle() {
-      $('.side-nav-toggle').on('click', function(e) {
-          $('.app').toggleClass('is-collapsed'), e.preventDefault();
-      });
-  }
-
-  sidePanelToggle() {
-      $('.side-panel-toggle').on('click', function(e) {
-          $('.side-panel').toggleClass('side-panel-open'), e.preventDefault();
-      });
+    app.init();
   }
 
 }
